5db8ee334a7e5c816a06588d616d6b5d72b82dd2
HRITHICK SUNDAR J <jayakumarhrithick2002@gmail.com>
2025-01-28 23:19:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,4 +123,5 @@ def move_processed_files(output_path, song_name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",4000))
+    app.run(host="0.0.0.0", port=port)
